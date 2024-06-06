a3db9101bcad37dbabace70f22ac76856a0ab1ab
Parsa Noori <parsanoori79@gmail.com>
2024-06-06 12:58:48 +0330

    added the computer lab results

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import TwoBlockchainsScheme
 
 
 def main():
-    simulation = PartialHomomorphyScheme.Simulation("nh1", quiet=False, random_speed_log_count=20, sleep_time=0.2,
-                                                    traffic_update_interval_in_seconds=30)
+    simulation = PartialHomomorphyScheme.Simulation("nh1", quiet=False, random_speed_log_count=20,
+                                                   sleep_time=0.2, traffic_update_interval_in_seconds=30)
     simulation.run()
     print(simulation.get_simulation_data())
     simulation.end_run()
